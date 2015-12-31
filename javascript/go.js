c0e001ce72bc29190b9a62f65c3533249431fbfb
Jimmy Petersen <jipe@dtic.dtu.dk>
2015-12-31 15:51:37 +0100

    Extract methods, optimize number of calls to render

--- javascript/go.js
+++ javascript/go.js
@@ -12,18 +12,10 @@ var go = (function () {
     this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
   };
 
-  Board.prototype.render = function () {
-    var size      = this.game.size;
-    var ctx       = this.ctx;
-    var canvas    = this.canvas;
-    var stepSize  = canvas.width / (size + 1);
-    var positions = this.game.positions;
-
+  Board.prototype.renderGrid = function (stepSize) {
+    var size = this.game.size;
+    var ctx  = this.ctx;
     var x,y;
-    var starPoints;
-    var i,j;
-    
-    ctx.clearRect(0,0,canvas.width,canvas.height);
 
     ctx.fillStyle = "#303030";
     ctx.beginPath();
@@ -39,6 +31,13 @@ var go = (function () {
       }
     }
     ctx.stroke();
+  };
+
+  Board.prototype.renderStarPoints = function (stepSize, radius) {
+    var size = this.game.size;
+    var ctx  = this.ctx;
+    var starPoints;
+    var i;
 
     if (size == 9) {
       starPoints = [[3,3], [7,3], [5,5], [3,7], [7,7]];
@@ -50,20 +49,46 @@ var go = (function () {
 
     for (i = 0; i < starPoints.length; i++) {
       ctx.beginPath();
-      ctx.arc(starPoints[i][0]*stepSize, starPoints[i][1]*stepSize, Math.max(3, stepSize / 22 * 2), 0, Math.PI*2); 
+      ctx.arc(starPoints[i][0]*stepSize, starPoints[i][1]*stepSize, radius, 0, Math.PI*2); 
       ctx.fill();
     }
-    
+  };
+
+  Board.prototype.renderPlacedStones = function (stepSize, radius) {
+    var size      = this.game.size;
+    var ctx       = this.ctx;
+    var positions = this.game.positions;
+
     for (i = 0; i < size; i++) {
       for (j = 0; j < size; j++) {
         if (positions[i][j] != -1) {
           ctx.fillStyle = positions[i][j] == 0 ? "rgb(0,0,0)" : "rgb(255,255,255)";
           ctx.beginPath();
-          ctx.arc((i+1)*stepSize, (j+1)*stepSize, stepSize / 2 - 1, 0, Math.PI*2);
+          ctx.arc((i+1)*stepSize, (j+1)*stepSize, radius, 0, Math.PI*2);
           ctx.fill();
         }
       }
     }
+  };
+
+  Board.prototype.render = function () {
+    var size      = this.game.size;
+    var ctx       = this.ctx;
+    var canvas    = this.canvas;
+    var stepSize  = canvas.width / (size + 1);
+    var positions = this.game.positions;
+
+    var x,y;
+    var starPoints;
+    var i,j;
+    
+    console.log("Render");
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    this.renderGrid(stepSize);
+    this.renderStarPoints(stepSize, Math.max(3, stepSize / 22 * 2));
+    this.renderPlacedStones(stepSize, stepSize / 2 - 1);
   };
 
   Board.prototype.attachUiControls = function (game) {
@@ -72,6 +97,7 @@ var go = (function () {
     var stepSize = canvas.width / (game.size + 1);
     var ctx      = this.ctx;
     var position;
+    var previousPosition;
 
     this.canvas.onmousemove = function (event) {
       position = {
@@ -82,12 +108,15 @@ var go = (function () {
       position.x = Math.min(19, Math.max(1, position.x));
       position.y = Math.min(19, Math.max(1, position.y));
 
-      board.render();    
-      if (game.currentPlayer.id != -1 && game.rules.evaluate(game, position.x, position.y).length == 0) {
-        ctx.fillStyle = game.currentPlayer.id == 0 ? "rgba(0,0,0,0.6)" : "rgba(255,255,255,0.6)";
-        ctx.beginPath();
-        ctx.arc(position.x*stepSize, position.y*stepSize, stepSize / 2 - 1, 0, Math.PI*2);
-        ctx.fill();
+      if (previousPosition == undefined || position.x !== previousPosition.x || position.y !== previousPosition.y) {
+        board.render();    
+        if (game.currentPlayer.id != -1 && game.rules.evaluate(game, position.x, position.y).length == 0) {
+          ctx.fillStyle = game.currentPlayer.id == 0 ? "rgba(0,0,0,0.6)" : "rgba(255,255,255,0.6)";
+          ctx.beginPath();
+          ctx.arc(position.x*stepSize, position.y*stepSize, stepSize / 2 - 1, 0, Math.PI*2);
+          ctx.fill();
+        }
+        previousPosition = position;
       }
     };
 
